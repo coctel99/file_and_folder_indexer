ff8449fe64d48fa5e29aa8568a04a2e071ebac31
Barabanov Alexander <sasha.bar99@gmail.com>
2022-01-26 20:53:52 +0300

    Add ability to read .docx files, add FileManager

--- file_and_folder_indexer/apps/file_reader/apps.py
+++ file_and_folder_indexer/apps/file_reader/apps.py
@@ -5,5 +5,5 @@ class FileReaderConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'file_and_folder_indexer.apps.file_reader'
     # allowed_file_extensions = [".txt", ".md", ".docx", ""]
-    allowed_file_extensions = [".txt"]
+    allowed_file_extensions = ['.txt', '.docx']
     encodings_queue = ['utf-8', 'Windows-1251', 'cp932', 'big5']
